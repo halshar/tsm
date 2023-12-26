fb20e64c171713fbd695c1701dfb6f4632e33ce5
Harshal Jadhav <bestt1315@gmail.com>
2023-12-27 02:15:07 +0530

    feat: fuzzy search the sessions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::io::Cursor;
 use std::process::{Command, Output, Stdio};
+
+use skim::prelude::{SkimItemReader, SkimOptionsBuilder};
+use skim::Skim;
 
 /// Fetch all tmux session names and return as a string.
 ///
@@ -44,9 +48,45 @@ fn execute_tmux_command(command: &str) -> Result<Output, std::io::Error> {
         .output()
 }
 
+/// Takes the session names as input and returns either the matched session
+/// name or the input query.
+fn session_name(input: String) -> Result<String, String> {
+    let options = SkimOptionsBuilder::default()
+        .height(Some("50%"))
+        .multi(false)
+        .color(Some("dark"))
+        .build()
+        .unwrap();
+
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(input));
+
+    let selected_item =
+        Skim::run_with(&options, Some(items)).ok_or_else(|| "Fuzzy finder error".to_owned())?;
+
+    if selected_item.is_abort {
+        return Err("cancelled".to_owned());
+    }
+
+    let val = selected_item.selected_items.get(0).map_or_else(
+        || selected_item.query.to_string(),
+        |item| item.text().to_string(),
+    );
+
+    Ok(val)
+}
+
 fn main() {
-    match all_sessions() {
-        Ok(all_sessions) => println!("all sessions: {}", all_sessions),
-        Err(err) => println!("{}", err),
+    let user_input = match all_sessions() {
+        Ok(all_sessions) => all_sessions,
+        Err(err) => {
+            println!("{}", err);
+            return;
+        }
+    };
+
+    match session_name(user_input) {
+        Ok(val) => println!("val: {}", val),
+        Err(err) => println!("err: {}", err),
     };
 }
