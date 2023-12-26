0d810e49ed2137977f41e3bc73920e060a02fcb7
Harshal Jadhav <bestt1315@gmail.com>
2023-12-27 02:59:59 +0530

    fix: added enum to handle the errors

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,48 @@ use std::process::{Command, Output, Stdio};
 use skim::prelude::{SkimItemReader, SkimOptionsBuilder};
 use skim::Skim;
 
+/// CustomErrors for command execution.
+enum TsmErrors {
+    /// The parsing of stdout into string failed.
+    NonUtf8Path,
+    /// The skim fuzzy finder command failed.
+    FuzzyFindError(String),
+    /// The tmux command failed.
+    CommandExecutionFailed(String),
+    /// The operation was cancelled.
+    OperationCancelled,
+}
+
 /// Fetch all tmux session names and return as a string.
 ///
 /// # Errors
 /// Returns an error if the command execution fails or if there are issues
 /// with UTF-8 conversion of the command's output.
-fn all_sessions() -> Result<String, String> {
+fn all_sessions() -> Result<String, TsmErrors> {
     // Command to list all the session name, the `-F` flag lists only name.
     let all_sessions_command = "tmux list-sessions -F #S";
 
-    match execute_tmux_command(all_sessions_command) {
-        Ok(output) => {
-            // if the command execution was successful return the list of session names
-            // else return the error
-            if output.status.success() {
-                match String::from_utf8(output.stdout) {
-                    Ok(all_sessions) => Ok(all_sessions),
-                    Err(e) => Err(format!(
-                        "Error: Command is not valid utf-8 string: {}",
-                        e.utf8_error()
-                    )),
-                }
-            } else {
-                match String::from_utf8(output.stderr) {
-                    Ok(e) => Err(format!("Error:- {}", e)),
-                    Err(e) => Err(format!(
-                        "Error: Command is not valid utf-8 string: {}",
-                        e.utf8_error()
-                    )),
-                }
-            }
-        }
-        Err(e) => Err(format!("Command execution failed: {}", e)),
+    let output = execute_tmux_command(all_sessions_command)?;
+    if output.status.success() {
+        String::from_utf8(output.stdout).map_err(|_| TsmErrors::NonUtf8Path)
+    } else {
+        String::from_utf8(output.stderr)
+            .map_err(|_| TsmErrors::CommandExecutionFailed("Command Failed".to_string()))
     }
 }
 
 /// Takes command as an input and returns the command's output.
-fn execute_tmux_command(command: &str) -> Result<Output, std::io::Error> {
-    let args: Vec<&str> = command.split(' ').skip(1).collect();
+fn execute_tmux_command(command: &str) -> Result<Output, TsmErrors> {
     Command::new("tmux")
-        .args(args)
+        .args(command.split(' ').skip(1))
         .stdin(Stdio::inherit())
         .output()
+        .map_err(|e| TsmErrors::CommandExecutionFailed(e.to_string()))
 }
 
 /// Takes the session names as input and returns either the matched session
 /// name or the input query.
-fn session_name(input: String) -> Result<String, String> {
+fn session_name(input: String) -> Result<String, TsmErrors> {
     let options = SkimOptionsBuilder::default()
         .height(Some("50%"))
         .multi(false)
@@ -61,11 +56,11 @@ fn session_name(input: String) -> Result<String, String> {
     let item_reader = SkimItemReader::default();
     let items = item_reader.of_bufread(Cursor::new(input));
 
-    let selected_item =
-        Skim::run_with(&options, Some(items)).ok_or_else(|| "Fuzzy finder error".to_owned())?;
+    let selected_item = Skim::run_with(&options, Some(items))
+        .ok_or_else(|| TsmErrors::FuzzyFindError("Error: Cannot select the option".to_string()))?;
 
     if selected_item.is_abort {
-        return Err("cancelled".to_owned());
+        return Err(TsmErrors::OperationCancelled);
     }
 
     let val = selected_item.selected_items.get(0).map_or_else(
@@ -76,17 +71,21 @@ fn session_name(input: String) -> Result<String, String> {
     Ok(val)
 }
 
-fn main() {
-    let user_input = match all_sessions() {
-        Ok(all_sessions) => all_sessions,
-        Err(err) => {
-            println!("{}", err);
-            return;
-        }
-    };
+fn run() -> Result<(), TsmErrors> {
+    let user_input = all_sessions()?;
+    let user_session_name = session_name(user_input)?;
+    println!("{user_session_name}");
+    Ok(())
+}
 
-    match session_name(user_input) {
-        Ok(val) => println!("val: {}", val),
-        Err(err) => println!("err: {}", err),
+fn main() {
+    match run() {
+        Ok(_) => (),
+        Err(e) => match e {
+            TsmErrors::NonUtf8Path => println!("Error: Command is not valid utf-8 string"),
+            TsmErrors::FuzzyFindError(e) => println!("Error: {e}"),
+            TsmErrors::CommandExecutionFailed(e) => println!("Error: {e}"),
+            TsmErrors::OperationCancelled => println!("Operation cancelled"),
+        },
     };
 }
